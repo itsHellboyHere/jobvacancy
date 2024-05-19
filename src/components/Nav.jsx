879d6ea58dce088ecb9b63353f624d451f1686ef
itsHellboHere <visalkr976@gmail.com>
2024-05-19 14:42:31 +0530

    added some padding in hero and navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import '../assets/Hero.css'
 const Nav = () => {
   return (
     <nav className='col w-screen'>
-      <div className=' align-element px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
+      <div className=' align-element px-4 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
         <h2 className='text-3xl font-bold'>
         Singh <span className='text-sky-200'>& </span> <span className='text-teal-600'>Sons</span>
         </h2>
